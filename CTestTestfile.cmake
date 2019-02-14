6ea664d7b31bd8e73ba946625a1736ff50f6d7a9
Tobi Adeyinka <adeyinka.oluwatobi@yahoo.com>
2019-02-14 01:32:41 +0100

    remove code coverage module

---
+++ CTestTestfile.cmake
@@ -0,0 +1,8 @@
+# CMake generated Testfile for 
+# Source directory: /Users/tobi/Desktop/final push/algorithms
+# Build directory: /Users/tobi/Desktop/final push/algorithms
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+add_test(NodeTests "/Users/tobi/Desktop/final push/algorithms/lib/node-tests")
+add_test(BinarySearchTests "/Users/tobi/Desktop/final push/algorithms/lib/binary-search-tests")
