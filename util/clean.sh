98f9419a56d39e9237297c8339de648a0e3fcd0e
Tobi Adeyinka <adeyinka.oluwatobi@yahoo.com>
2019-02-14 01:04:37 +0100

    add cmake coverge module

--- util/clean.sh
+++ util/clean.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 
 rm -rf bin lib CMakeFiles CMakeScripts Testing cmake_install.cmake CMakeCache.txt
-rm -rf CTestTestfile.cmake Makefile cmake-build-debug algorithms.cbp
+rm -rf CTestTestfile.cmake Makefile cmake-build-debug algorithms.cbp external
